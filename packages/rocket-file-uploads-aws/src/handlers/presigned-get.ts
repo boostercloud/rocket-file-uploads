c368dcf1dbcd6db6e0216e1dec6f1d48ceae61e3
charlietfe <carlos@theagilemonkeys.com>
2023-01-27 09:31:45 +0000

    test

--- packages/rocket-file-uploads-aws/src/handlers/presigned-get.ts
+++ packages/rocket-file-uploads-aws/src/handlers/presigned-get.ts
@@ -2,6 +2,8 @@ import { BoosterConfig } from '@boostercloud/framework-types'
 import { RocketFilesUserConfiguration } from '@boostercloud/rocket-file-uploads-types'
 import * as AWS from 'aws-sdk'
 
+/* TEST */
+
 export async function presignedGet(
   config: BoosterConfig,
   rocketFilesUserConfiguration: RocketFilesUserConfiguration,
